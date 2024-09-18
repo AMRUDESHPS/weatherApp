991a68085574f97c073b991ec9c23344879f5f64
AMRUDESH PS <126590195+AMRUDESHPS@users.noreply.github.com>
2024-09-18 12:30:14 +0530

    server and backend code developed

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.render("index", {weather: null, err: null});
+});
+
+app.get("/weather",async (req, res)=>{
+    const city = req.query.city;
+    const APIKey = "2b60422df03c091fc14c3e57f06c69a1";
+
+    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    let weather;
+    let err = null;
+
+    try{
+        const response = await axios.get(APIUrl);
+        weather = response.data;
+        console.log("its info : "+weather);
+    }catch(err){
+        weather = null;
+        err = "Error, Please try again";
+    }
+    res.render("index", {weather, err});
+});
+
+app.listen("3000",()=>{
+    console.log("App/Server running on port 3000");
+})
